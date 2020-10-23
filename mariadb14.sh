386db2e2f5b3096d8d24de94a3ef3cc906748311
Roy Bellingan <admin@seisho.us>
2020-10-23 17:02:00 +0100

    Update mariadb14.sh

--- mariadb14.sh
+++ mariadb14.sh
@@ -3,8 +3,19 @@ zypper -n addrepo --gpgcheck --refresh https://yum.mariadb.org/10.4/opensuse/15/
 zypper -n --gpg-auto-import-keys refresh
 zypper in -f MariaDB-server-10.4.11-1
 zypper in MariaDB-backup
+
+#per motivi MISTICI non è presente come default di usare jemalloc, e questo ha causato problemi 
+#sopratutto se usi rocksdb...
+zypper in jemalloc
+echo 'Environment="LD_PRELOAD=/usr/lib64/libjemalloc.so.2"' >> /usr/lib/systemd/system/mariadb.service
+systemctl daemon-reload
+
 systemctl start mariadb
 chkconfig mariadb on
 
 #in case you are doing an update also execute
 # mysql_upgrade 
+
+#verificare sia usato jemalloc tramite 
+#lsof -p $(pidof mysqld) | grep "jemalloc"
+#SHOW VARIABLES LIKE 'version_malloc_library';
