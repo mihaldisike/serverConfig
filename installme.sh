97c745d9f3df7d4dfb3a2d3779054ac5e11eb099
Roy Bellingan <admin@seisho.us>
2020-10-25 21:25:19 +0000

    Update installme.sh

--- installme.sh
+++ installme.sh
@@ -1,7 +1,7 @@
 #at one point or another I will use all of that
 #-n means non interactive
 zypper -n in sshfs moreutils strace vnstat sysstat iotop percona-toolkit
-zypper -n in net-snmp net-snmp-devel autossh tree
+zypper -n in net-snmp net-snmp-devel autossh tree tcpdump
 zypper rm nano
 
 rcvnstatd start
