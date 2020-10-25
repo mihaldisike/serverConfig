16ad5831ef8e9af1890334c17eddf14b821bfb11
Roy Bellingan <admin@seisho.us>
2020-10-25 15:22:32 +0000

    Update php74.sh

--- php74.sh
+++ php74.sh
@@ -7,8 +7,7 @@ zypper -n addrepo --gpgcheck -p 90 http://download.opensuse.org/repositories/dev
 #zypper -n addrepo --gpgcheck -p 90 https://download.opensuse.org/repositories/server:http/openSUSE_Leap_15.1/server:http.repo
 
 zypper -n --gpg-auto-import-keys refresh
-zypper -n install php7 php7-devel php7-fpm composer php7-mbstring php7-fileinfo php7-curl php7-mysql php7-soap php7-
-he php7-shmop php7-tidy
+zypper -n install php7 php7-devel php7-fpm composer php7-mbstring php7-fileinfo php7-curl php7-mysql php7-soap php7-he php7-shmop php7-tidy
 
 zypper -n in nginx
 #in some case this is quite usefull to enable the (just at the beginning)
