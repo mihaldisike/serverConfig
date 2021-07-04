bc87cbf70e223d76ae674281a3a44e56a18732b9
Roy Bellingan <admin@seisho.us>
2021-07-04 02:01:43 +0200

    Update gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -1,3 +1,3 @@
 #qt need to be installed by itself
 
-zypper in cmake cpp10 gcc gcc10 gcc10-c++ gcc7 gcc7-c++ gcc-c++ git okteta clang-tidy clazy
+zypper in cmake cpp10 gcc gcc10 gcc10-c++ gcc7 gcc7-c++ gcc-c++ git okteta clazy
