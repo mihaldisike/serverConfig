572a1fe6be5a059a931e6d1b49782641426deae4
Roy Bellingan <admin@seisho.us>
2021-10-09 01:00:25 +0100

    Create gcc.sh
    x

--- gcc.sh
+++ gcc.sh
@@ -1,3 +1,3 @@
 #qt need to be installed by itself
 
-zypper in cmake cpp10 gcc gcc10 gcc10-c++ gcc7 gcc7-c++ gcc-c++ git okteta clazy
+zypper in cmake cpp10 gcc gcc10 gcc10-c++ gcc7 gcc7-c++ gcc-c++ git okteta clazy cmake
