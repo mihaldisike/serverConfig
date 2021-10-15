2f4879dcd06009224434d370b405aeefad8cc96b
Roy Bellingan <admin@seisho.us>
2021-10-15 13:52:42 +0100

    Update gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -1,3 +1,7 @@
 #qt need to be installed by itself
 
 zypper in cmake cpp10 gcc gcc10 gcc10-c++ gcc7 gcc7-c++ gcc-c++ git okteta clazy cmake binutils-gold
+
+#In 99.99% is ok to use ld-gold instead of bfg
+rm /etc/alternatives/ld
+ln -s /usr/bin/ld.gold /etc/alternatives/ld
