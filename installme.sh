553117899936402c32a855e3475493ade6ddb687
Roy Bellingan <admin@seisho.us>
2021-11-19 14:20:31 +0000

    Update installme.sh

--- installme.sh
+++ installme.sh
@@ -10,6 +10,10 @@ zypper -n in net-snmp net-snmp-devel munin-node percona-toolkit
 #the world is now a better place by 0.000000000000001%
 zypper rm nano
 
+#to be able to replicate an ssh command on multiple machine, remember the two config file so is not UUUGLY!
+#Put those in your home .Xresources  and csshconf
+zypper in clusterssh
+
 rcvnstatd start
 chkconfig vnstatd on
 
