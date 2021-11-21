15426bd6232a68649c46f2793b37d130291a29da
Roy Bellingan <admin@seisho.us>
2021-11-21 16:48:37 +0000

    Update installme.sh

--- installme.sh
+++ installme.sh
@@ -1,7 +1,7 @@
 #at one point or another I will use all of that
 #-n means non interactive
 zypper -n in sshfs moreutils strace vnstat sysstat iotop
-zypper -n in autossh tree tcpdump curlftpfs netcat
+zypper -n in autossh tree tcpdump curlftpfs netcat gnu_parallel
 
 
 #only for server
