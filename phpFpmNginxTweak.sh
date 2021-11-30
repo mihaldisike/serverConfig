7980a05054765667af5150b26099b6caef48dc97
Roy Bellingan <admin@seisho.us>
2021-11-30 12:29:49 +0000

    Update phpFpmNginxTweak.sh

--- phpFpmNginxTweak.sh
+++ phpFpmNginxTweak.sh
@@ -1,8 +1,9 @@
 After having cloned the related repositories 
 
-Add inside 
+Add 
 
-vi /etc/nginx/fastcgi_params
+printf '\n# Il super error handler scoperto da Ivano' >> /etc/nginx/fastcgi_params
+printf '\nfastcgi_param  PHP_VALUE   "auto_prepend_file=/srv/www/phpCommon/errorHandler.php";' >> /etc/nginx/fastcgi_params
 
-# Il super error handler scoperto da Ivano
-fastcgi_param  PHP_VALUE   "auto_prepend_file=/srv/www/phpCommon/errorHandler.php";
+printf '\n#srly ? how do they think is going to work whitout that ?' >> /etc/nginx/fastcgi_params
+printf '\nfastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;' >> /etc/nginx/fastcgi_params
