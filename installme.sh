0b09bfb235e16f72dc17d5f5d44a5e7a27d933ed
Roy Bellingan <admin@seisho.us>
2021-11-30 13:31:11 +0000

    Update installme.sh

--- installme.sh
+++ installme.sh
@@ -8,7 +8,9 @@ zypper -n in autossh tree tcpdump curlftpfs netcat gnu_parallel
 zypper -n in net-snmp net-snmp-devel munin-node percona-toolkit
 
 #the world is now a better place by 0.000000000000001%
-zypper rm nano
+zypper rm nano 
+#never used
+zypper rm joe
 
 #to be able to replicate an ssh command on multiple machine, remember the two config file so is not UUUGLY!
 #Put those in your home .Xresources  and csshconf
