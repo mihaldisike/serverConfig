2663c90b7cce5e27dc40f45ac870489cf7b3678c
Roy Bellingan <admin@seisho.us>
2022-03-04 03:19:04 +0000

    Update gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -8,3 +8,5 @@ rm /etc/alternatives/ld
 ln -s /usr/bin/ld.gold /etc/alternatives/ld
 
 #mold instead, faster but still some minor annoyances with valgrind or slow debug in some cases 
+
+zypper in libcurl-devel jemalloc-devel libzip-devel libmaxminddb-devel libdw-devel libmariadb-devel
