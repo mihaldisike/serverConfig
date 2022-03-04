3018e2ee8f88fb67b32b32e37d1b4a9e5a6f7b51
Roy Bellingan <admin@seisho.us>
2022-03-04 02:49:07 +0000

    Update gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -6,3 +6,5 @@ zypper in cpp10 gcc gcc10 gcc10-c++ gcc7 gcc7-c++ gcc-c++
 #In 99.99% is ok to use ld-gold instead of bfg
 rm /etc/alternatives/ld
 ln -s /usr/bin/ld.gold /etc/alternatives/ld
+
+#mold instead, faster but still some minor annoyances with valgrind or slow debug in some cases 
