3259673cfb3983ff6b66a0bd60c67081113e45d4
Balázs Óvári <37852896+simplex-fx@users.noreply.github.com>
2022-03-22 19:01:39 +0100

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,2 +1,5 @@
 When you add a key from another system remember to do 
  ssh-add
+ apt-get install libboost-all-dev
+ apt-get install apt-get install libcurl4-openssl-dev
+ apt-get install gcc-11 g++-11
