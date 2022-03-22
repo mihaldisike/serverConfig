65415cbda5be37701cf39fcedd7510aed810306f
Balázs Óvári <37852896+simplex-fx@users.noreply.github.com>
2022-03-22 19:07:29 +0100

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,3 +3,6 @@ When you add a key from another system remember to do
  apt-get install libboost-all-dev
  apt-get install apt-get install libcurl4-openssl-dev
  apt-get install gcc-11 g++-11
+ apt-get install libmaxminddb-dev
+ 
+ /opt/Qt/MaintenanceTool  install 5.15.2 too
