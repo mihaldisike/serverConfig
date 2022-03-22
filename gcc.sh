7c48db4c2764ef368abaea275be814f487f8e797
Roy Bellingan <admin@seisho.us>
2022-03-22 19:33:15 +0000

    Update gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -1,12 +1,12 @@
 #qt need to be installed by itself
 
 zypper in cmake git okteta clazy cmake binutils-gold mold
-zypper in cpp10 gcc gcc10 gcc10-c++ gcc7 gcc7-c++ gcc-c++
+zypper in cpp11 gcc gcc11 gcc11-c++ gcc-c++
 
-#In 99.99% is ok to use ld-gold instead of bfg
+#In 99.99% is ok to use ld-gold instead of bfg, ATM I (Roy) the only problem is linking mariadb
 rm /etc/alternatives/ld
 ln -s /usr/bin/ld.gold /etc/alternatives/ld
 
-#mold instead, faster but still some minor annoyances with valgrind or slow debug in some cases 
+#mold instead, faster but still some minor annoyances with valgrind or slow debug in some cases use at your own risk
 
 zypper in libcurl-devel jemalloc-devel libzip-devel libmaxminddb-devel libdw-devel libmariadb-devel
