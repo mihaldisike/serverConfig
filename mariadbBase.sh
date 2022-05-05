5042e3f72676d4fbd526dd67057d16458024a795
Roy Bellingan <admin@seisho.us>
2022-05-05 14:11:10 +0100

    Create mariadbBase.sh

---
+++ mariadbBase.sh
@@ -0,0 +1,10 @@
+zypper rm mariadb mariadb-client mariadb-server mariadb-tools
+
+systemctl start mariadb
+#autostart
+chkconfig mariadb on
+
+mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root mysql
+
+echo "now setup the user to connect https://s22.trott.pw/dev_wiki/index.php?title=MySQL_Create_User"
+
