06050520ff5d8a172ec3c77cede77cbfdf0e58bb
Roy Bellingan <admin@seisho.us>
2022-08-12 00:55:43 +0100

    Update installme.sh

--- installme.sh
+++ installme.sh
@@ -10,11 +10,6 @@ zypper in --no-recommends  perf
 #only for server
 zypper -n in net-snmp net-snmp-devel munin-node percona-toolkit
 
-#the world is now a better place by 0.000000000000001%
-zypper -n rm nano 
-#never used, just got scared of seeing a folder called joe in etc with inside shell.sh!!!
-zypper -n rm joe
-
 #to be able to replicate an ssh command on multiple machine, remember the two config file so is not UUUGLY!
 #Put those in your home .Xresources  and csshconf
 zypper -n in clusterssh
