796b03bfd67cc720e7ddbf01e09ac3f30e337ee5
Roy Bellingan <admin@seisho.us>
2022-08-24 10:09:08 +0100

    Update gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -6,7 +6,6 @@ zypper in cpp gcc gcc-c++ gcc-c++
 #mold instead, faster but still some minor annoyances with valgrind in some cases use at your own risk
 #remember to change the .pro to use it (normally put in the mkSpec.pri
 # QMAKE_LFLAGS += -B /usr/bin/mold
-zypper in mold
 
 zypper in libcurl-devel jemalloc-devel libzip-devel libmaxminddb-devel libdw-devel libmariadb-devel boost-devel libxml++-devel
 
