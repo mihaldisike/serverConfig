2999a5dea9a49a4000780f9c26c4e60825d3a2f7
Roy Bellingan <admin@seisho.us>
2022-11-17 19:08:16 +0000

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -5,8 +5,8 @@ location ~ \.php$ {
     include fastcgi_params;
     fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
     fastcgi_pass 127.0.0.1:9000;
-    fastcgi_index  index.php;
 }
+index index.php;
 EOF
 
 
