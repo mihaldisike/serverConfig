c51ce3258689134ac34c52e607714bf1fbc30d64
Roy Bellingan <admin@seisho.us>
2023-04-03 18:12:46 +0100

    Update gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -1,12 +1,12 @@
 #qt need to be installed by itself
 
-zypper in cmake git okteta clazy cmake mold
-zypper in cpp gcc gcc-c++ gcc-c++
+zypper in -n cmake git okteta clazy cmake mold
+zypper in -n cpp gcc gcc-c++ gcc-c++
 
 #mold instead, faster but still some minor annoyances with valgrind in some cases use at your own risk
 #remember to change the .pro to use it (normally put in the mkSpec.pri
 # QMAKE_LFLAGS += -B /usr/bin/mold
 
-zypper in libcurl-devel jemalloc-devel libzip-devel libmaxminddb-devel libdw-devel libmariadb-devel boost-devel libxml++-devel
+zypper in -n libcurl-devel jemalloc-devel libzip-devel libmaxminddb-devel libdw-devel libmariadb-devel boost-devel libxml++-devel
 
 #boost version be sure to have at least the 71
